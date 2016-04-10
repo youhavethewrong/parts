e26fd1a355f83e7ed0d668773fac5d27ccfae296
negated <youhavethewrong@gmail.com>
2016-04-10 15:01:29 -0500

    More items in the inventory.

--- src/cljs/parts/items.cljs
+++ src/cljs/parts/items.cljs
@@ -2,17 +2,28 @@
 
 (def inventory
   [
-   {:name "2011 Toyota Corolla"
-    :parts {"Motor Oil" "0w20 Synthetic"
-            "Oil Filter" [""]}}
-   {:name "1982 Honda CB750F"
-    :parts {"Motor Oil" "10w40 Synthetic"
-            "Oil Filter" [""]}}
+   {:name "2011 Toyota Corolla LE"
+    :parts {"Motor Oil" ["0w20 Synthetic"]
+            "Oil Filter" ["Taco 12"]}}
+   {:name "2006 Toyota Camry"
+    :parts {"Motor Oil" ["10w30 Conventional"]
+            "Oil Filter" ["Taco 12"]}}
+   {:name "1982 Honda CB750F SuperSport"
+    :parts {"Motor Oil" ["10w40 Synthetic"]
+            "Oil Filter" ["Taco 12"]}}
    {:name "1996 Simplicity Lawn Tractor"
-    :parts {"Motor Oil" "10w30 Conventional"
+    :parts {"Motor Oil" ["10w30 Conventional"]
             "Oil Filter" ["Wix 51348"]}}
    {:name "2008 Yard Machines Snow Blower"
-    :parts {"Motor Oil" ""}}
-
+    :parts {"Motor Oil" ["10w30 Conventional"]}}
+   {:name "2011 Trimmer"
+    :parts {"Motor Oil" ["10w30 Conventional (40:1)"]}}
+   {:name "2014 Leaf Blower"
+    :parts {"Motor Oil" ["10w30 Conventional (40:1)"]}}
+   {:name "2004 Whirlpool Refrigerator"
+    :parts {"Water/Ice Filter" ["TBD"]
+            "Ice Maker" ["TBD"]}}
+   ;; {:name ""
+   ;;  :parts {"" [""]}}
    ]
 )
