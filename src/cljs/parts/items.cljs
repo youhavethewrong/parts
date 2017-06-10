b6985319651040e44e0772ca534e991e5756839d
ESC <youhavethewrong@gmail.com>
2017-06-09 19:58:49 -0500

    Air filter for corolla, another oil filter for honda.

--- src/cljs/parts/items.cljs
+++ src/cljs/parts/items.cljs
@@ -5,13 +5,15 @@
    {:name "2011 Toyota Corolla LE"
     :parts {"Motor Oil" ["0w20 Synthetic"]
             "Oil Filter" ["FRAM CH10358" "FRAM TG10358" "Toyota 04152-YZZA6" "Purolator L16311"
-                          "K&N PS-7021" "Wix 57064" "ACDelco PF1768" "Mann ML 1048" "Hastings #LF640"]}}
+                          "K&N PS-7021" "Wix 57064" "ACDelco PF1768" "Mann ML 1048" "Hastings #LF640"]
+            "Air Filter" ["FRAM CA9360" ]}}
    {:name "2006 Toyota Camry"
     :parts {"Motor Oil" ["10w30 Conventional"]
             "Oil Filter" ["FRAM HM4386"]}}
    {:name "1982 Honda CB750F SuperSport"
     :parts {"Motor Oil" ["10w40 Synthetic"]
             "Oil Filter" ["FRAM CH6009"
+                          "K&N 401"
                           "Mobil1 M1MC-171"]}}
    {:name "1996 Simplicity Lawn Tractor"
     :parts {"Motor Oil" ["10w30 Conventional"]
